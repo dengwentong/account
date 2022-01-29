6b3268e716a81484d558d5110140879171e569f3
dengwentong <dengwentong@hotmail.com>
2022-01-29 08:19:45 +0800

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
       user: 'root',
       host: 'dwt.webok.net',
       ref: 'origin/master',
-      repo: 'git@github.com:dengwentong/juejue-vite-h5.git',
+      repo: 'git@github.com:dengwentong/account.git',
       path: '/mnt/sdb1/var/www/account/juejue-vite-h5',
       'post-deploy': 'git reset --hard && git checkout master && git pull && npm i --production=false && pm2 startOrReload ecosystem.config.js', // -production=false 下载全量包
       env: {
